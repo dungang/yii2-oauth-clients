552a7d714be17ea58fcd95c1c6e3f768773f0b3f
dungang <dungang@126.com>
2017-03-23 10:35:21 +0800

    modify composer.josn description

---
+++ IAuth.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace dungang\oauth;
+
+/**
+ * Oauth Interface
+ * @author xjflyttp <xjflyttp@gmail.com>
+ */
+interface IAuth
+{
+
+    /**
+     *
+     * @return []
+     */
+    public function getUserInfo();
+
+    /**
+     *
+     * @return mixed
+     */
+    public function getOpenid();
+}
